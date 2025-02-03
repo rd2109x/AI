372f87b6a31fb2c5a464a03ee9d9981ab02500b3
todd.dole <todd.dole@gmail.com>
2025-02-03 17:01:28 -0600

    Fixed bugs, improved logic for discard and meld so that it should no longer forfeit.

--- main.py
+++ main.py
@@ -6,22 +6,25 @@ import uvicorn
 import os
 import signal
 import logging
+#import pytest
 
 """
-By Todd Dole, Revision 1.1
+By Todd Dole, Revision 1.2
 Written for Hardin-Simmons CSCI-4332 Artificial Intelligence
 Revision History
 1.0 - API setup
 1.1 - Very basic test player
+1.2 - Bugs fixed and player improved, should no longer forfeit
 """
 
 # TODO - Change the PORT and USER_NAME Values before running
 DEBUG = True
-PORT = 8001
+PORT = 8002
 USER_NAME = "jsmith"
 # TODO - change your method of saving information from the very rudimentary method here
 hand = [] # list of cards in our hand
 discard = [] # list of cards organized as a stack
+cannot_discard = ""
 
 # set up the FastAPI application
 app = FastAPI()
@@ -59,7 +62,9 @@ async def start_hand(hand_info: HandInfo):
     # TODO - Your code here
     global hand
     global discard
-    hand = hand_info.hand.split(" ").sort()
+    discard = []
+    hand = hand_info.hand.split(" ")
+    hand.sort()
     logging.info("2p hand started, hand is " + str(hand))
     return {"status": "OK"}
 
@@ -72,6 +77,7 @@ def process_events(event_text):
 
         if ((USER_NAME + " draws") in event_line or (USER_NAME + " takes") in event_line):
             print("In draw, hand is "+str(hand))
+            print("Drew "+event_line.split(" ")[-1])
             hand.append(event_line.split(" ")[-1])
             hand.sort()
             print("Hand is now "+str(hand))
@@ -80,6 +86,8 @@ def process_events(event_text):
             discard.insert(0, event_line.split(" ")[-1])
         if ("takes" in event_line): # remove a card from discard pile
             discard.pop(0)
+        if " Ends:" in event_line:
+            print(event_line)
 
 # data class used to receive data from API POST
 class UpdateInfo(BaseModel):
@@ -94,18 +102,46 @@ async def update_2p_game(update_info: UpdateInfo):
     '''
     # TODO - Your code here - update this section if you want
     process_events(update_info.event)
+    print(update_info.event)
     return {"status": "OK"}
 
 @app.post("/draw/")
 async def draw(update_info: UpdateInfo):
     ''' Game Server calls this endpoint to start player's turn with draw from discard pile or draw pile.'''
+    global cannot_discard
     # TODO - Your code here - everything from here to end of function
     process_events(update_info.event)
     if len(discard)<1: # If the discard pile is empty, draw from stock
+        cannot_discard = ""
         return {"play": "draw stock"}
-    if any(discard[0][0] in s for s in hand): # if our hand contains a matching card, take it
+    if any(discard[0][0] in s for s in hand):
+        cannot_discard = discard[0] # if our hand contains a matching card, take it
         return {"play": "draw discard"}
     return {"play": "draw stock"} # Otherwise, draw from stock
+
+def get_of_a_kind_count(hand):
+    of_a_kind_count = [0, 0, 0, 0]  # how many 1 of a kind, 2 of a kind, etc in our hand
+    last_val = hand[0][0]
+    count = 0
+    for card in hand[1:]:
+        cur_val = card[0]
+        if cur_val == last_val:
+            count += 1
+        else:
+            of_a_kind_count[count] += 1
+            count = 0
+        last_val = cur_val
+    of_a_kind_count[count] += 1  # Need to get the last card fully processed
+    return of_a_kind_count
+
+def get_count(hand, card):
+    count = 0
+    for check_card in hand:
+        if check_card[0] == card[0]: count += 1
+    return count
+
+#def test_get_of_a_kind_count():
+#    assert get_of_a_kind_count(["2S", "2H", "2D", "7C", "7D", "7S", "7H", "QC", "QD", "QH", "AH"]) == [1, 0, 2, 1]
 
 @app.post("/lay-down/")
 async def lay_down(update_info: UpdateInfo):
@@ -113,68 +149,72 @@ async def lay_down(update_info: UpdateInfo):
     # TODO - Your code here - everything from here to end of function
     global hand
     global discard
+    global cannot_discard
     process_events(update_info.event)
-    of_a_kind_count = [0, 0, 0, 0] # how many 1 of a kind, 2 of a kind, etc in our hand
-    last_val = hand[0][0]
-    count = 0
-    for card in hand[1:]:
-        cur_val = card[0]
-        if cur_val == last_val:
-            count+=1
-        else:
-            of_a_kind_count[count] += 1
-            count=0
-        last_val = cur_val
-    if (count!=0): of_a_kind_count[count]+=1 # Need to get the last card fully processed if it is a match to the previous
-    if (of_a_kind_count[0]+of_a_kind_count[1]) > 1:
+    of_a_kind_count = get_of_a_kind_count(hand)
+    if (of_a_kind_count[0]+(of_a_kind_count[1]*2)) > 1:
+        print("Need to discard")
         # Too many unmeldable cards, need to discard
 
         # If we have a 1 of a kind, discard the highest
+
         if (of_a_kind_count[0]>0):
-            for i in range(len(hand)-1,-1, -1):
+            print("Discarding a single card")
+            logging.info("Discarding a single card")
+
+            # edge case - the last card is 1 of a kind
+            if (hand[-1][0] != hand[-2][0]):
+                logging.info("Discarding " + hand[-1])
+                return {"play": "discard " + hand.pop()}
+
+            for i in range(len(hand)-2,-1, -1):
                 if (i==0):
                     logging.info("Discarding "+hand[0])
                     return {"play":"discard "+hand.pop(0)}
-                if hand[i][0] != hand[i-1][0]:
+                if hand[i][0] != hand[i-1][0] and hand[i][0] != hand[i+1][0]:
                     logging.info("Discarding "+hand[i])
                     return {"play":"discard "+hand.pop(i)}
 
-        # discard the highest 2 of a kind
-            i=len(hand)-1
-            while (i>0):
-                if (i==1):
-                    logging.info("Discarding "+hand[1])
-                    return {"play":"discard "+hand.pop(1)}
-                if hand[i][0] != hand[i-2][0]:
+        elif (of_a_kind_count[1]>=1):
+            print("Discarding two of a kind, cannot_discard = "+cannot_discard)
+            for i in range(len(hand)-1,-1, -1):
+                if (hand[i]!=cannot_discard and get_count(hand,hand[i]) == 2):
                     logging.info("Discarding "+hand[i])
-                    return {"play":"discard "+hand.pop(i)}
-                while hand[i][0] == hand[i-1][0]: i-=1 #skip over meldable sets
-                i-=1
+                    return {"play": "discard " + hand.pop(i)}
+
+            logging.info("Discarding " + hand[i])
+            return {"play": "discard " + hand.pop(i)}
+
 
     # We should be able to meld.
 
-    # First, find the card we discard
+    # First, find the card we discard - if needed
     discard_string = ""
     print(of_a_kind_count)
-    # TODO - Dole - Need to add edge case for last card being a one-of-a-kind
+
+
+
     if (of_a_kind_count[0] > 0):
-        for i in range(len(hand)-1, -1, -1):
-            if (i == 0):
-                discard_string = " discard " + hand.pop(0)
-                break
-            if hand[i][0] != hand[i - 1][0]:
-                discard_string = " discard " + hand.pop(i)
-                break
+        if hand[-1][0] != hand[-2][0]:
+            discard_string = " discard " + hand.pop()
+        else:
+            for i in range(len(hand)-2, -1, -1):
+                if (i == 0):
+                    discard_string = " discard " + hand.pop(0)
+                    break
+                if hand[i][0] != hand[i - 1][0] and hand[i][0] != hand[i + 1][0]:
+                    discard_string = " discard " + hand.pop(i)
+                    break
 
     # generate our list of meld
     play_string = ""
     last_card = ""
     while (len(hand) > 0):
         card = hand.pop(0)
-        if (str(card) != last_card):
+        if (str(card)[0] != last_card):
             play_string += "meld "
         play_string += str(card) + " "
-        last_card = str(card)
+        last_card = str(card)[0]
 
     # remove the extra space, and add in our discard if any
     play_string = play_string[:-1]
@@ -198,11 +238,13 @@ if __name__ == "__main__":
         url = "http://127.0.0.1:16200/test"
 
         # TODO - Change logging.basicConfig if you want
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(filename="RummyPlayer.log", format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',level=logging.INFO)
     else:
         url = "http://127.0.0.1:16200/register"
         # TODO - Change logging.basicConfig if you want
-        logging.basicConfig(level=logging.WARNING)
+        logging.basicConfig(filename="RummyPlayer.log", format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',level=logging.WARNING)
 
     payload = {
         "name": USER_NAME,
